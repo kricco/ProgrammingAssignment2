73bfc7635963544f171bbcc874031f63e821ce4a
kricco <kristjan@kitsemets.net>
2015-12-27 22:48:16 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
+#define makeChacheMatrix function
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+#define functions to set the value of the vector, get the value of the vector, set the value of inverse matrix and get the value of inverse matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
@@ -7,17 +9,20 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setsolve <- function(solve) m <<- solve
         getsolve <- function() m
+#create the special "vector" to store the list of functions
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
-
+#define cacheSolve function
 cacheSolve <- function(x, ...) {
+#check if the value has been calculated before, if yes, get the cached data and return its value ie inverse matrix
         m <- x$getsolve()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+#if the value has not been calculated before, do the calculation and return the inverse matrix
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
